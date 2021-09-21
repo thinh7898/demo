7c78ce15a4338d8d5f2a1e00b64e96f1294f8100
thinh7898 <nd.thinh7898@gmail.com>
2021-09-21 14:38:37 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào bạn Thế ! 🖐🖐🖐',
+    introDesc: `T có cái này hay cực :v, nhấn vô để biết nhé !!!`,
+    btnIntro: 'OK',
+    title: 'M thấy bản thân mình có ngu si không :)) ? ',
+    desc: 'trả lời thật lòng nha, ko đc out đó :> ',
+    btnYes: 'ngu vkl ra luôn ý chứ ',
+    btnNo: 'Không, đương nhiên là t rất thông minh rồi  ! ',
+    question:'m chọn câu này là hơi bị đúng đấy 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! ',
+    reply: 'ngu thì ngu thôi 😊😊  ',
+    mess: 'Thế khi học nhiều vào :3 ',
+    messDesc: '💬💬 inbox luôn để có thêm kiến thức nha. 💬💬',
+    btnAccept: 'Ok lun :v',
+    messLink: 'https://www.facebook.com/kjiu.ngo'
+}
